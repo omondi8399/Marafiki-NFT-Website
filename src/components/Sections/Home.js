057b0397914789c175059c72d3eb054a8f75f021
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-18 18:13:46 +0300

    updated Home component

--- src/components/Sections/Home.js
+++ src/components/Sections/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import TypeWriterText from '../TypeWriterText'
 import CoverVideo from '../CoverVideo'
+import RoundTextBlack from '../../assets/Rounded-Text-Black.png'
 
 const Section = styled.section`
 min-height: ${props => `calc(100vh - ${props.theme.navHeight})`  };
@@ -29,12 +30,60 @@ justify-content: center;
 align-items: center;
 `
 
+const rotate = keyframes`
+100%{
+  transform: rotate(1turn);
+}
+`
+
+const Round = styled.div`
+position:absolute;
+bottom: 2rem;
+right: 90%;
+width: 6rem;
+height: 6rem;
+border: 1px solid ${props => props.theme.text};
+border-radius: 50px;
+
+img{
+  width: 100%;
+  height: auto;
+  animation:${rotate} 6s linear infinite reverse;
+}
+`
+
+const Circle = styled.span`
+width: 3rem;
+height: 3rem;
+display: flex;
+justify-content: center;
+align-items: center;
+border-radius: 50%;
+
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+background-color: ${props => props.theme.text};
+color: ${props => props.theme.body};
+font-size:${props => props.theme.fontxl};
+`
+
+
 const Home = () => {
   return (
     <Section>
       <Container>
         <Box><TypeWriterText /> </Box>
         <Box><CoverVideo />  </Box>
+
+        <Round>
+            <Circle>
+              &#x2193;
+            </Circle>
+            <img src={RoundTextBlack} alt="NFT" />
+        </Round>
+
       </Container>
     </Section>
   )
