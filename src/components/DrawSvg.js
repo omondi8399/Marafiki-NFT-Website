59ddee3989a8bfa13fd5034476d135a238f4626f
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-18 18:11:08 +0300

    added DrawSvg component

---
+++ src/components/DrawSvg.js
@@ -0,0 +1,62 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import styled from 'styled-components'
+import Vector from '../Icons/Vector'
+import  gsap  from 'gsap'
+import  ScrollTrigger  from 'gsap/ScrollTrigger'
+
+const VectorContainer = styled.div`
+position: absolute;
+top: 0.5rem;
+left: 50%;
+transform: translateX(-50%);
+width: 100%;
+height: 100%;
+overflow: hidden;
+
+svg{
+    width: 100%;
+    height: 100%;
+}
+`
+
+const DrawSvg = () => {
+    const ref = useRef(null)
+
+    gsap.registerPlugin(ScrollTrigger);
+    useLayoutEffect(() => {
+        let element = ref.current;
+
+        let svg = document.getElementsByClassName("svg-path")[0];
+
+        const length = svg.getBoundingClientRect()
+
+        //positioning of svg drawing
+        svg.style.strokeDasharray = length;
+
+        //Hide svg before scrolling start
+        svg.style.strokeDashoffset = length;
+
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger:element,
+                start: "top center",
+                end:"bottom bottom",
+                onUpdate: (self) => {
+
+                }
+            }
+        })
+
+        return () => {
+
+        };
+    })
+
+  return (
+    <VectorContainer ref={ref}>
+        <Vector />
+    </VectorContainer>
+  )
+}
+
+export default DrawSvg
