4dbf38a4df731675636df446c96cafac94806af9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-20 14:46:21 +0300

    Updated Team section

--- src/components/Sections/Team.js
+++ src/components/Sections/Team.js
@@ -112,7 +112,7 @@ const MemberComponent = ({img, name=" ",position=" "}) => {
 
 const Team = () => {
   return (
-    <Section>
+    <Section id='team'>
       <ConfettiComponent />
       <Title>Team</Title>
       <Container>
