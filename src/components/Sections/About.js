02e81fa145ec4be67c65d859bd898d47ba57474b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-21 12:19:39 +0300

    updated about section

--- src/components/Sections/About.js
+++ src/components/Sections/About.js
@@ -22,14 +22,38 @@ margin: 0 auto;
 display: flex;
 justify-content: center;
 align-items: center;
+
+@media (max-width: 70em){
+  width:80%;
+}
+@media (max-width: 64em){
+  width:100%;
+  flex-direction: column;
+
+  &>*:last-child{
+    width: 80%;
+  }
+}
+@media (max-width: 40em){
+  
+  &>*:last-child{
+    width: 90%;
+  }
+}
 `
 const Box = styled.div`
 width: 50%;
 height: 100%;
+min-height: 60vh;
 display: flex;
 flex-direction:column;
 justify-content: center;
 align-items: center;
+
+@media (max-width: 40em){
+  min-height: 50vh;
+}
+
 `
 const Title = styled.h2`
 font-size: ${props => props.theme.fontxxl};
@@ -39,6 +63,17 @@ color: ${props => props.theme.body};
 align-self: flex-start;
 width: 80%;
 margin: 0 auto;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align: center;
+}
+@media (max-width: 40em){
+  font-size: ${props => props.theme.fontxxl};
+}
+@media (max-width: 30em){
+  font-size: ${props => props.theme.fontlg};
+}
 `
 
 const SubText = styled.p`
@@ -48,6 +83,18 @@ align-self: flex-start;
 width: 80%;
 margin: 1rem auto;
 font-weight: 400;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align: center;
+  font-size: ${props => props.theme.fontmd};
+}
+@media (max-width: 40em){
+  font-size: ${props => props.theme.fontmd};
+}
+@media (max-width: 30em){
+  font-size: ${props => props.theme.fontsm};
+}
 `
 const SubTextLight = styled.p`
 font-size: ${(props) => props.theme.fontmd};
@@ -56,12 +103,32 @@ align-self: flex-start;
 width: 80%;
 margin: 1rem auto;
 font-weight: 400;
+
+@media (max-width: 64em){
+  width: 100%;
+  text-align: center;
+  font-size: ${props => props.theme.fontsm};
+}
+@media (max-width: 40em){
+  font-size: ${props => props.theme.fontsm};
+}
+@media (max-width: 30em){
+  font-size: ${props => props.theme.fontxs};
+}
 `
 const ButtonContainer =styled.div`
 width:80%;
 margin: 1rem auto;
+display: flex;
 align-self: flex-start;
 
+@media (max-width: 64em){
+  width: 100%;
+
+  button{
+    margin: 0 auto;
+  }
+}
 `
 
 
@@ -72,7 +139,7 @@ const About = () => {
         <Box> <Carousel/> </Box>
         <Box>
           <Title>
-            Welcome To The Makutano Club.
+            Welcome To The <br /> Makutano Club.
           </Title>
           <SubText>
             The MAKUTANO CLUB is a private collection of NFTs—unique digital collectibles. The Weirdos are stored as ERC-721 tokens on the Ethereum blockchain and hosted on IPFS.
