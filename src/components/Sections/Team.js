7bd98219cc58598bcccf69fda9c86e59a3887f3b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-21 12:22:17 +0300

    updated Team section

--- src/components/Sections/Team.js
+++ src/components/Sections/Team.js
@@ -29,6 +29,10 @@ align-items: center;
 margin: 1rem auto;
 border-bottom: 2px solid ${props => props.theme.text};
 width: fit-content;
+
+@media (max-width: 40em) {
+  font-size: ${props => props.theme.fontxl};
+}
 `;
 
 const Container = styled.div`
@@ -39,6 +43,14 @@ display: flex;
 justify-content: space-between;
 align-items: center;
 flex-wrap: wrap;
+
+@media (max-width: 64em) {
+  width: 80%;
+}
+@media (max-width: 48em) {
+  width: 90%;
+  justify-content: center;
+}
 `
 const Item = styled.div`
 width: calc(20rem -4vw);
@@ -57,6 +69,10 @@ border-radius: 20px;
   img{
     transform: translateY(-2rem) scale(1.2);
   }
+}
+
+@media (max-width: 30em) {
+  width: 70vw;
 }
 
 `
